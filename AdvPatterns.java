282d58901281e8d5cca4728e131871db1ddcfbdd
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-08-30 19:15:26 +0530

    pattern

--- AdvPatterns.java
+++ AdvPatterns.java
@@ -95,5 +95,41 @@ public class AdvPatterns {
         //     }
         //     System.out.println();
         // }
+
+        // ------ 6. hollow Rhombus ------
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print("  ");
+        //     }
+        //     for(int j=1; j<=n; j++){
+        //         if (i==1||j==1||i==n||j==n) {
+        //             System.out.print("* ");
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        // ------ 7. half pyramid ------
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print(j+ " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // ------ 8. inverted half pyramid ------
+        // for(int i=1; i<=n; i++){
+        //     for(int j=n; j>=i; j--){
+        //         System.out.print(i+ " ");
+        //     }
+        //     System.out.println();
+        // }
+
     }
 }
