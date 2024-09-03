ec7d74258de5a44478ee5d8bb67c498003ad3798
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-09-04 00:30:40 +0530

    string dsa

---
+++ Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    public static void main(String[] args) {
+        String str = "bhargav";
+        String str2 = "bhargav";
+        System.out.println(str);
+        
+        
+        for(int i=0; i<str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+
+        System.out.println(str.substring(2));
+    }
+}
