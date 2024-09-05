bd1f6cf6e55c9a2af7660401d223c0c2f8d8454a
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-09-05 22:33:21 +0530

    bit

---
+++ BitManipulation.java
@@ -0,0 +1,54 @@
+/**
+ * BitManipulation
+ */
+public class BitManipulation {
+    public static void main(String[] args) {
+
+    // ---------- GET BIT ----------
+
+        // int n= 5;
+        // int pos = 2;
+        // int BitMask = 1 << pos;
+
+        // if ((BitMask & n) == 0) {
+        //     System.out.println("Get Bit Zero");
+        // }
+        // else{
+        //     System.out.println("Get Bit One");
+        // }
+
+    // ---------- SET BIT ----------
+
+        // int n= 5;
+        // int pos = 3;
+        // int BitMask = 1 << pos;
+        // int ope = BitMask | n; // OR
+        // System.out.println(ope);
+
+    // ---------- CLEAR BIT ----------
+
+        // int n= 5;
+        // int pos = 2;
+        // int BitMask = 1 << pos;
+        // int newBM = ~(BitMask);  // NOT
+        // int res = newBM & n;  // AND 
+        // System.out.println(res);
+
+    // ---------- UPDATE BIT ----------
+
+        // int n= 5;
+        // int pos = 2;
+        // int BitMask = 1 << pos;
+        // int ope = 0;
+        // if (ope == 1) {
+        //     int res = BitMask | n;  // OR
+        //     System.out.println(res);
+        // }
+        // else{
+        //     int newBM = ~(BitMask);  // NOT
+        //     int res = newBM & n;  // AND
+        //     System.out.println(res);
+        // }
+
+    }
+}
