f7785a0b37681d89cf35e5045d6bb89836f64ce2
Bhuro11 <bhargavtalpada2004@gmail.com>
2025-01-02 17:57:54 +0530

    solution

--- LeetCodeSolution.js
+++ LeetCodeSolution.js
@@ -1,19 +1,11 @@
-function permutation(str, permu, result) {
-    if (str.length === 0) {
-        result.push(permu); // Add the current permutation to the result array
-        return;
-    }
-    for (let i = 0; i < str.length; i++) {
-        let currChar = str.charAt(i); 
-        let newStr = str.substring(0, i) + str.substring(i + 1); // Corrected method name
-        permutation(newStr, permu + currChar, result); // Pass the result array in the recursive call
-    }
-}
+let arr = [1,2,3,4,5]
 
-function getPermutations(str) {
-    let result = [];
-    permutation(str, "", result); // Start the recursion with an empty permutation string
-    return result;
-}
+let totalSum = arr.reduce((i, sum) => i + sum, 0)
 
-console.log(getPermutations("ABC")); // Output: ["ABC", "ACB", "BAC", "BCA", "CAB", "CBA"]
+let minNum = Math.min(...arr)
+let maxNum = Math.max(...arr)
+
+let min = totalSum - maxNum;
+let max = totalSum - minNum;
+
+console.log(min, max)
